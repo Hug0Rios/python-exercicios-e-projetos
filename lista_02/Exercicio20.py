1fdcc6e6b0ebd2b7ec18cea412fd62b419d49d50
Hugo Rios <hugocarvalhorios@gmail.com>
2026-04-01 20:05:02 -0300

    Alterações para corrigir erros de lògica.

--- lista_02/Exercicio20.py
+++ lista_02/Exercicio20.py
@@ -1,23 +1,20 @@
 #20.Desenvolva um algoritmo para que, dados dois valores inteiros entre 1 e 10 lidos,
 # calcule e imprima: a média dos números caso a soma deles for menor que 8, seu produto caso a soma seja igual a 8
 # ou a divisão do maior pelo menor caso a soma dos valores for maior que 8.
-numero_1= int(input('Digite um primeiro valor inteiro: '))
-numero_2= int(input('Digite um segundo valor inteiro: '))
-maior = numero_1
-if numero_1 <0 and numero_1 >10:
-    print('Valor invalido!')
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite um numero: "))
+if numero1 < 1 or numero1 > 10 or numero2 < 1 or numero2 > 10:
+    print("Numeros invalidos")
 else:
-    if numero_2 <0 and numero_2 >10:
-        print('Valor invalido!')
-if (numero_1+numero_2)<8:
-    resultado = (numero_1+numero_2)/2
-    print('Média igual a:',resultado)
-else:
-    if (numero_1+numero_2)==8:
-        resultado = (numero_1*numero_2)
-        print('O produto é igual a:',resultado)
-if ((numero_1+numero_2)>8):
-    if numero_2<numero_1:
-        print('A divisão é igual a',numero_1/numero_2)
+    soma = numero1 + numero2
+    if soma < 8:
+        resultado = soma / 2
     else:
-        print('A divisão é igual a',numero_2/numero_1)
+        if soma == 8:
+            resultado = numero1 * numero2
+        else:
+            if numero1 > numero2:
+                resultado = numero1 / numero2
+            else:
+                resultado = numero2 / numero1
+    print(resultado)
